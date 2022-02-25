99785ec3a7cd35de50534483b89c50b8414194b6
Maria Rosario <mariarosariojohansson@gmail.com>
2022-02-25 16:18:46 +0100

    navbar in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home'
+import Checkout from './pages/Checkout'
+import Header from './components/Header'
+import Products from './pages/Products'
+import Cart from './components/Cart'
 
 function App() {
   return (
     <div className="App">
-
       
+        <BrowserRouter>
+        <Header />
+      
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Products' element={<Products/>}/>
+          <Route path='/Cart' element={<Cart/>}/>
+          <Route path='/Checkout' element={<Checkout/>}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav className='navbar'>
+      <div className='nav-1'>
+        <Link to="/">Home</Link> | <Link to="/Products">Products</Link>
+     </div>
+     <div className='nav-2'>
+       <Link to="/Cart">Cart</Link> | <Link to="/Checkout">Checkout</Link>
+     </div>
+    </nav>
   )
 }
 
-export default Header
+export default Header;
