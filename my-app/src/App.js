60f307577b4f890975d262e5065bbf6d7feb7240
morgan <morgan.planeryd@gmail.com>
2022-03-07 10:16:03 +0100

    nudå

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Product from './pages/Product';
+import Footer from './components/Footer';
+import Checkout from './pages/Checkout';
 
 function App() {
+
+  
+
+
+
+
   return (
     <div className="App">
-
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Product />} />
+      <Route path="/product" element={<Product />} />
+     
+    </Routes>
+    <Footer />    
+  </BrowserRouter>,
+  </div>
   );
 }
 
